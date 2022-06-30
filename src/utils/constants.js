58b78296d3db2efcf78ef0a8c6b2b8d440e165a0
j. jillian <judith.mediosdigitales@gmail.com>
2022-06-30 14:36:37 +0200

    fix url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-export const BASE_API_URL = 
-process.env.NODE_ENV == 'production'
+export const BASE_API_URL = process.env.NODE_ENV === 'production'
 ? 'https://make-the-difference.herokuapp.com'
-:'http://localhost:5005'
+: 'http://localhost:5005'
