5224f3ac42c31358684a4b36e14abee72b362fef
j. jillian <judith.mediosdigitales@gmail.com>
2022-06-30 14:14:10 +0200

    add local url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const BASE_API_URL = 'https://make-the-difference.herokuapp.com'
+export const BASE_API_URL = 
+process.env.NODE_ENV == 'production'
+? 'https://make-the-difference.herokuapp.com'
+:'http://localhost:5005'
