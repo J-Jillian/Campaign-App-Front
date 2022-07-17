3fe6c44bb2c1f217c373ea1add265d0e51f3a746
j. jillian <judith.mediosdigitales@gmail.com>
2022-07-17 21:26:27 +0200

    change name

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const BASE_API_URL = process.env.NODE_ENV === 'production'
-? 'https://make-the-difference.herokuapp.com'
+? 'https://make-the-difference.netlify.app'
 : 'http://localhost:5005'
+// 'https://make-the-difference.herokuapp.com'
